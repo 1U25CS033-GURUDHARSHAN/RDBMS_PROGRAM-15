e08dfe254baa747bfe32d16e03eb9ccd77a7fa34
1U25CS033-GURUDHARSHAN <guru139663@gmail.com>
2026-09-25 12:53:38 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,16 @@
+DELIMITER //
 
+CREATE PROCEDURE check_marks()
+BEGIN
+    DECLARE marks INT DEFAULT 65;
+
+    IF marks >= 50 THEN
+        SELECT 'PASS' AS Result;
+    ELSE
+        SELECT 'FAIL' AS Result;
+    END IF;
+END //
+
+DELIMITER ;
+
+CALL check_marks();
